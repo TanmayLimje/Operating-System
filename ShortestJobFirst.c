a8ccf948a8ff6fba4a199ebd29b75b72a0bc96c1
Tanmay Limje <103094622+TanmayLimje@users.noreply.github.com>
2023-11-20 18:17:04 +0530

    Update ShortestJobFirst.c

--- ShortestJobFirst.c
+++ ShortestJobFirst.c
@@ -1,163 +1,121 @@
-//sjf
+// shortest job first
 
 #include <stdio.h>
-
 #include <stdlib.h>
 
+typedef struct
+{
+	int pid, bt, at, wt, tat;
+}Process;
 
+void swap(Process *a, Process *b)
+{
+	Process temp = *a;
+	*a = *b;
+	*b = temp;
+}
 
-typedef struct {
+void sortByAT(Process processes[], int n)
+{
+	for(int i = 0; i < n-1; i++)
+	{
+		for(int j = 0; j < n-i-1; j++)
+		{
+			if(processes[j].at > processes[j+1].at)
+			{
+				swap(&processes[j], &processes[j+1]);
+			}
+		}
+	}
+}
 
-    int process_id;
+void SJF(Process processes[], int n)
+{
+	int currTime = 0;
+	int totalWT = 0;
+	int totalTAT = 0;
 
-    int burst_time;
+	for (int i = 0; i < n; i++)
+	{
+		processes[i].wt = currTime - processes[i].at;
+	
+		if(processes[i].wt < 0)
+		{
+			currTime = processes[i].at;
+			processes[i].wt = 0;
+		}
+	
+		processes[i].tat = processes[i].wt + processes[i].bt;
+		currTime += processes[i].bt;
+	
+		totalWT += processes[i].wt;
+		totalTAT += processes[i].tat;
+	}
 
-    int arrival_time;
-
-    int waiting_time;
-
-    int turnaround_time;
-
-} Process;
-
-
-
-void swap(Process *a, Process *b) {
-
-    Process temp = *a;
-
-    *a = *b;
-
-    *b = temp;
-
+	printf("\nProcesses\tBT\tAT\tWT\tTAT\n");
+	for(int i = 0; i < n; i++)
+	{
+		printf("%d\t\t%d\t%d\t%d\t%d\n", processes[i].pid, processes[i].bt, processes[i].at, processes[i].wt, processes[i].tat);
+	}
+	
+    printf("\nAvg wt: %.2f\n", (float)totalWT / n);
+	printf("Avg tat: %.2f\n", (float)totalTAT / n);
 }
 
 
 
-void sortProcessesByArrivalTime(Process processes[], int n) {
+int main()
+{
+	int n;
 
-    for (int i = 0; i < n - 1; i++) {
+	printf("Enter no of processes:");
+	scanf("%d",&n);
 
-        for (int j = 0; j < n - i - 1; j++) {
+	Process processes[n];
 
-            if (processes[j].arrival_time > processes[j + 1].arrival_time) {
+	printf("Enter bt and at for each processes:");
 
-                swap(&processes[j], &processes[j + 1]);
+	for(int i = 0; i < n; i++)
+	{
+		processes[i].pid = i+1;
 
-            }
+		printf("\nProcess %d - BT:", i+1);
+		scanf("%d", &processes[i].bt);
 
-        }
+		printf("Process %d - AT:", i+1);
+		scanf("%d", &processes[i].at);
 
-    }
+	}
 
+	sortByAT(processes,n);
+	SJF(processes,n);
+
+	return 0;
 }
 
 
-
-void sjfScheduling(Process processes[], int n) {
-
-    int currentTime = 0;
-
-    int totalWaitingTime = 0, totalTurnaroundTime = 0;
-
-
-
-    for (int i = 0; i < n; i++) {
-
-        processes[i].waiting_time = currentTime - processes[i].arrival_time;
-
-
-
-        if (processes[i].waiting_time < 0) {
-
-            currentTime = processes[i].arrival_time;
-
-            processes[i].waiting_time = 0;
-
-        }
-
-
-
-        processes[i].turnaround_time = processes[i].waiting_time + processes[i].burst_time;
-
-        currentTime += processes[i].burst_time;
-
-
-
-        totalWaitingTime += processes[i].waiting_time;
-
-        totalTurnaroundTime += processes[i].turnaround_time;
-
-    }
-
-
-
-    printf("\nProcess\tBurst Time\tArrival Time\tWaiting Time\tTurnaround Time\n");
-
-    for (int i = 0; i < n; i++) {
-
-        printf("%d\t%d\t\t%d\t\t%d\t\t%d\n", processes[i].process_id, processes[i].burst_time,
-
-               processes[i].arrival_time, processes[i].waiting_time, processes[i].turnaround_time);
-
-    }
-
-
-
-    printf("Average Waiting Time: %.2f\n", (float)totalWaitingTime / n);
-
-    printf("Average Turnaround Time: %.2f\n", (float)totalTurnaroundTime / n);
-
-}
-
-
-
-int main() {
-
-    int n;
-
-
-
-    printf("Enter the number of processes: ");
-
-    scanf("%d", &n);
-
-
-
-    Process processes[n];
-
-
-
-    printf("Enter burst time and arrival time for each process:\n");
-
-    for (int i = 0; i < n; i++) {
-
-        processes[i].process_id = i + 1;
-
-        printf("Process %d - Burst Time: ", i + 1);
-
-        scanf("%d", &processes[i].burst_time);
-
-        printf("Process %d - Arrival Time: ", i + 1);
-
-        scanf("%d", &processes[i].arrival_time);
-
-    }
-
-
-
-    // Sort processes by arrival time
-
-    sortProcessesByArrivalTime(processes, n);
-
-
-
-    // Execute SJF Scheduling algorithm
-
-    sjfScheduling(processes, n);
-
-
-
-    return 0;
-
-}
+// ---------------------------------------------------------------------------------------------
+// OUTPUT:
+    
+// tanmay@tanmay-VirtualBox:~/os$ gcc SJF.c
+// tanmay@tanmay-VirtualBox:~/os$ ./a.out
+
+// Enter no of processes:3
+// Enter bt and at for each processes:
+
+// Process 1 - BT:22
+// Process 1 - AT:1
+
+// Process 2 - BT:44
+// Process 2 - AT:2
+
+// Process 3 - BT:55
+// Process 3 - AT:1
+
+// Processes	BT	AT	WT	TAT
+// 1		    22	1	0	22
+// 3		    55	1	22	77
+// 2		    44	2	76	120
+
+// Avg wt: 32.67
+// Avg tat: 73.00
