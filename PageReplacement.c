a5ee1071d9b9ee6421a648d2c78797919592be08
Tanmay Limje <103094622+TanmayLimje@users.noreply.github.com>
2023-11-21 09:01:19 +0530

    Update PageReplacement.c

--- PageReplacement.c
+++ PageReplacement.c
@@ -1,295 +1,358 @@
-// page replacement
+#include<stdio.h>
 
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdlib.h>
 
-#define MAX_F 10
 
-void printFrames(int f[], int capacity)
-{
-	for(int i = 0; i < capacity; i++)
-	{
-		if(f[i] == -1)
-			printf("- ");
-		else
-			printf("%d(%d) ", f[i], i+1);
-	}
 
-	printf("\n");
-}
+void printFrames(int frames[], int frameSize) {
 
-void fifo(int pages[], int n, int capacity)
-{
-	int frames[MAX_F];
-	int faults = 0;
-	int frameIndex = 0;
+    for (int i = 0; i < frameSize; ++i) {
 
-	printf("\n FIFO: \n");
+        if (frames[i] == -1) {
 
-	for(int i = 0; i < n; i++)
-	{
-		int found = 0;
+            printf("X ");
 
-		for(int j = 0; j < capacity; j++)
-		{
-			if(frames[j] == pages[i])
-			{
-				found = 1;
-				break;
-			}
-		}
+        } else {
 
-		if(!found)
-		{
-			faults++;
+            printf("%d ", frames[i]);
 
-			if(frameIndex < capacity)
-			{
-				frames[frameIndex++] = pages[i];
-			}
-			else
-			{
-				int oldest = 0;
-		
-				for(int j = 1; j < capacity; j++)
-				{
-					if(frames[j] < frames[oldest])
-					{
-						oldest = j;
-					}
-				}
-				frames[oldest] = pages[i];
-			}
-		}
-			printf("\nFrames: \n");
-		printFrames(frames, capacity);
-	}
-	printf("\nTotal page faults: %d\n", faults);
-}
+        }
 
-void LRU (int pages[], int n, int capacity)
-{
-	int frames[MAX_F];
-	int lru[MAX_F];
-	int faults = 0;
-	int i,j;
-	
-	printf("\nLRU:\n");
-	
-	for(i = 0; i < n; i++)
-	{
-		int found = 0;
-		
-		for( j = 0 ; j < capacity; j++)
-		{
-			if(frames[j] = pages[i])
-			{
-				found = 1;
-				lru[j] = i;
-				break;
-			}
-		}
-		
-		if(!found)
-		{
-			faults++;
-	
-			printf("MISS: page %d \n", pages[i]);
-			
-			if(i < capacity)
-			{
-				frames[i] = pages[i];
-				lru[i] = i;
-			}
-			else
-			{
-				int least = 0;				
+    }
 
-				for( j = 0; j < capacity; j++)
-				{
-					if(lru[j] < lru[least])
-					{
-						least = j;
-					}
-				}
-				
-				frames[least] = pages[i];
-				lru[least] = i;
+    printf("\n");
 
-			}
-		}
-		else
-		{
-			printf("HIT: page %d \n", pages[i]);
-		}
-	}
-
-	printf("Total page faults : %d \n", faults);
-	
-}
-
-void optimal(int pages[], int n, int capacity)
-{
-	int frames[MAX_F];
-	int faluts = 0;
-	int i,j,k;	
-
-	printf("\nOptimal:\n");	
-
-	for(i = 0; i < n; i++)
-	{
-		int found = 0;		
-
-		for(j = 0; j < capacity; j++)
-		{
-			if(frames[j] == pages[i])
-			{
-				found = 1;
-				break;
-			}
-		}		
-
-		if(!found)
-		{
-			faluts++;			
-
-			printf("MISS: page %d \n", pages[i]);			
-
-			if (i < capacity)
-			{
-				frames[i] = pages[i];
-			}
-			else
-			{
-				int index[MAX_F];				
-
-				for(j = 0; j < capacity; j++)
-				{
-					index[j] = -1;
-					
-					for( k = i+1; k < n; k++)
-					{
-						if(frames[j] == pages[i])
-						{
-							index[j] = k;
-							break;
-						}
-					}
-				}				
-
-				int furthest = 0;			
-
-				for (j = 0; j < capacity; j++)
-				{
-					if(index[j] == -1 || index[j] > index[furthest])
-					{
-						furthest = j;
-					}
-				}		
-
-				frames[furthest] = pages[i];
-			}	
-		}
-		else
-		{
-			printf("HIT: page %d \n",pages[i]);
-		}
-	}
-	printf("Total page faluts: %d \n", faluts);
 }
 
 
 
-int main()
-{
-	int pages[] = {7,0,1,2,0,3,0,4,2,3,0,3,1,2};
-	int n = sizeof(pages) / sizeof(pages[0]);
-	int capacity = 3;
+void FCFS(int pages[], int frameSize, int n) {
 
-	fifo(pages,n,capacity);
-	LRU(pages,n,capacity);
-	optimal(pages,n,capacity);
+    int frames[frameSize];
 
-	return 0;
+    int counter = 0;
+
+    int pageFaults = 0;
+
+
+
+    for (int i = 0; i < frameSize; ++i) {
+
+        frames[i] = -1;
+
+    }
+
+
+
+    for (int i = 0; i < n; ++i) {
+
+        int pageExists = 0;
+
+        for (int j = 0; j < frameSize; ++j) {
+
+            if (frames[j] == pages[i]) {
+
+                pageExists = 1;
+
+                break;
+
+            }
+
+        }
+
+
+
+        if (pageExists == 0) {
+
+            frames[counter] = pages[i];
+
+            ++counter;
+
+            ++pageFaults;
+
+            if (counter >= frameSize) {
+
+                counter = 0;
+
+            }
+
+        }
+
+        printf("Frames (FCFS): ");
+
+        printFrames(frames, frameSize);
+
+    }
+
+
+
+    printf("Number of page faults using FCFS: %d\n\n", pageFaults);
+
 }
 
-//-------------------------------------------------------------------
-//  FIFO: 
 
-// Frames: 
-// 7(1) 0(2) 0(3) 
 
-// Frames: 
-// 7(1) 0(2) 0(3) 
+void LRU(int pages[], int frameSize, int n) {
 
-// Frames: 
-// 7(1) 1(2) 0(3) 
+    int frames[frameSize];
 
-// Frames: 
-// 7(1) 1(2) 2(3) 
+    int counter = 0;
 
-// Frames: 
-// 7(1) 0(2) 2(3) 
+    int pageFaults = 0;
 
-// Frames: 
-// 7(1) 3(2) 2(3) 
+    int recent[] = {-1, -1, -1};
 
-// Frames: 
-// 7(1) 3(2) 0(3) 
 
-// Frames: 
-// 7(1) 3(2) 4(3) 
 
-// Frames: 
-// 7(1) 2(2) 4(3) 
+    for (int i = 0; i < frameSize; ++i) {
 
-// Frames: 
-// 7(1) 3(2) 4(3) 
+        frames[i] = -1;
 
-// Frames: 
-// 7(1) 0(2) 4(3) 
+    }
 
-// Frames: 
-// 7(1) 3(2) 4(3) 
 
-// Frames: 
-// 7(1) 1(2) 4(3) 
 
-// Frames: 
-// 7(1) 2(2) 4(3) 
+    for (int i = 0; i < n; ++i) {
 
-// Total page faults: 13
+        int pageExists = 0;
 
-// LRU:
-// HIT: page 7 
-// MISS: page 0 
-// HIT: page 1 
-// HIT: page 2 
-// MISS: page 0 
-// HIT: page 3 
-// MISS: page 0 
-// HIT: page 4 
-// HIT: page 2 
-// HIT: page 3 
-// MISS: page 0 
-// HIT: page 3 
-// HIT: page 1 
-// HIT: page 2 
-// Total page faults : 4 
+        for (int j = 0; j < frameSize; ++j) {
 
-// Optimal:
-// MISS: page 7 
-// HIT: page 0 
-// MISS: page 1 
-// MISS: page 2 
-// HIT: page 0 
-// MISS: page 3 
-// HIT: page 0 
-// MISS: page 4 
-// MISS: page 2 
-// MISS: page 3 
-// HIT: page 0 
-// HIT: page 3 
-// MISS: page 1 
-// MISS: page 2 
-// Total page faluts: 9
+            if (frames[j] == pages[i]) {
+
+                pageExists = 1;
+
+                recent[j] = i;
+
+                break;
+
+            }
+
+        }
+
+
+
+        if (pageExists == 0) {
+
+            int leastRecentIndex = 0;
+
+            for (int j = 1; j < frameSize; ++j) {
+
+                if (recent[j] < recent[leastRecentIndex]) {
+
+                    leastRecentIndex = j;
+
+                }
+
+            }
+
+            frames[leastRecentIndex] = pages[i];
+
+            recent[leastRecentIndex] = i;
+
+            ++pageFaults;
+
+        }
+
+        printf("Frames (LRU): ");
+
+        printFrames(frames, frameSize);
+
+    }
+
+
+
+    printf("Number of page faults using LRU: %d\n\n", pageFaults);
+
+}
+
+
+
+void Optimal(int pages[], int frameSize,int n) {
+
+    int frames[frameSize];
+
+    int pageFaults = 0;
+
+
+
+    for (int i = 0; i < frameSize; ++i) {
+
+        frames[i] = -1;
+
+    }
+
+
+
+    for (int i = 0; i < n; ++i) {
+
+        int pageExists = 0;
+
+        for (int j = 0; j < frameSize; ++j) {
+
+            if (frames[j] == pages[i]) {
+
+                pageExists = 1;
+
+                break;
+
+            }
+
+        }
+
+
+
+        if (pageExists == 0) {
+
+            int maxDistance = -1;
+
+            int replaceIndex = -1;
+
+            for (int j = 0; j < frameSize; ++j) {
+
+                int futureIndex = 0;
+
+                for (int k = i + 1; k < 20; ++k) {
+
+                    if (frames[j] == pages[k]) {
+
+                        futureIndex = k;
+
+                        break;
+
+                    }
+
+                }
+
+                if (futureIndex == 0) {
+
+                    replaceIndex = j;
+
+                    break;
+
+                } else {
+
+                    if (futureIndex > maxDistance) {
+
+                        maxDistance = futureIndex;
+
+                        replaceIndex = j;
+
+                    }
+
+                }
+
+            }
+
+            frames[replaceIndex] = pages[i];
+
+            ++pageFaults;
+
+        }
+
+        printf("Frames (Optimal): ");
+
+        printFrames(frames, frameSize);
+
+    }
+
+
+
+    printf("Number of page faults using Optimal: %d\n\n", pageFaults);
+
+}
+
+
+
+int main() {
+
+    int pages[20];
+
+    int n;
+
+    printf("Enter the size of string \n");
+
+    scanf("%d",&n);
+
+    printf("Enter the reference string:\n");
+
+    for (int i = 0; i < n; ++i) {
+
+        scanf("%d", &pages[i]);
+
+    }
+
+
+
+    int frameSize = 3;
+
+
+
+    FCFS(pages, frameSize,n);
+
+    LRU(pages, frameSize,n);
+
+    Optimal(pages, frameSize,n);
+
+
+
+    return 0;
+
+}
+
+//-----------------------------------------------------------------------------------------------
+// output:
+//-----------------------------------------------------------------------------------------------
+
+// tanmay@tanmay-VirtualBox:~/os$ gcc testPR.c 
+// tanmay@tanmay-VirtualBox:~/os$ ./a.out
+
+
+// Enter the size of string 
+
+// 5
+
+// Enter the reference string:
+
+// 12 3 55 3 12   
+
+// Frames (FCFS): 12 X X 
+
+// Frames (FCFS): 12 3 X 
+
+// Frames (FCFS): 12 3 55 
+
+// Frames (FCFS): 12 3 55 
+
+// Frames (FCFS): 12 3 55 
+
+// Number of page faults using FCFS: 3
+
+
+
+// Frames (LRU): 12 X X 
+
+// Frames (LRU): 12 3 X 
+
+// Frames (LRU): 12 3 55 
+
+// Frames (LRU): 12 3 55 
+
+// Frames (LRU): 12 3 55 
+
+// Number of page faults using LRU: 3
+
+
+
+// Frames (Optimal): 12 X X 
+
+// Frames (Optimal): 12 3 X 
+
+// Frames (Optimal): 12 3 55 
+
+// Frames (Optimal): 12 3 55 
+
+// Frames (Optimal): 12 3 55 
+
+// Number of page faults using Optimal: 3
+
